eee6e94c134dfe0f1fa728f1fc8849e459778570
Petunia-AI <api@aipetunia.com>
2026-06-10 00:52:29 -0500

    Revert "fix: use inset+object-fit-cover on mux-video instead of translate trick"
    This reverts commit 11fd6100fd8f09e28ec3e60a062dd0a804205767.

--- src/components/site/MuxVideo.tsx
+++ src/components/site/MuxVideo.tsx
@@ -36,10 +36,16 @@ export default function MuxVideo({ playbackId, style }: MuxVideoProps) {
         style={{
           display: "block",
           position: "absolute",
-          inset: 0,
-          width: "100%",
-          height: "100%",
+          top: "50%",
+          left: "50%",
+          width: "177.78vh",
+          minWidth: "100%",
+          height: "56.25vw",
+          minHeight: "100%",
+          transform: "translate(-50%, -50%)",
           objectFit: "cover",
+          margin: 0,
+          padding: 0,
           ...style,
         }}
       />
